a90c82eee0e1b44584248661ccde37b2f490e928
Caro <silconito@gmail.com>
2021-07-04 21:43:39 -0300

    more cafeterias

--- script.js
+++ script.js
@@ -1,22 +1,70 @@
 const cafeterias = [
-    {
-      "nombre": "Old day's",
-      "dirección": "Olga Cossettini 1182, Puerto madero"
-    },
-    {
-      "nombre": "FOKO Recoleta",
-      "dirección": "Arenales 2630, Recoleta"
-    },
-    {
-      "nombre": "PANI",
-      "dirección": "Vicente López 2056"
-    },
-    {
-      "nombre": "El Gato Negro",
-      "dirección": "Av. Corrientes 1669,"
-    }
-  ]
-
+  {
+    "nombre": "Old day's",
+    "direccion": "Olga Cossettini 1182",
+    "barrio": "Puerto madero"
+  },
+  {
+    "nombre": "FOKO Recoleta",
+    "direccion": "Arenales 2630",
+    "barrio": "Recoleta"
+  },
+  {
+    "nombre": "PANI",
+    "direccion": "Vicente López 2056",
+    "barrio": "Recoleta"
+  },
+  {
+    "nombre": "El Gato Negro",
+    "direccion": "Av. Corrientes 1669",
+    "barrio": "Monserrat"
+  },
+  {
+    "nombre": "Café Tortoni",
+    "direccion": "Av. de Mayo 825",
+    "barrio": "San Nícolas"
+  },
+  {
+    "nombre": "Café Registrado",
+    "direccion": "Costa Rica 5901",
+    "barrio": "Palermo"
+  },
+  {
+    "nombre": "Coffee Town",
+    "direccion": "Bolivar 976",
+    "barrio": "San Telmo"
+  },
+  {
+    "nombre": "Vive Café",
+    "direccion": "Costa Rica 5772",
+    "barrio": "Palermo"
+  },
+  {
+    "nombre": "Lattente",
+    "direccion": "Thames 1891",
+    "barrio": "Palermo"
+  },
+  {
+    "nombre": "Nucha",
+    "direccion": "O'higgins 1400",
+    "barrio": "Belgrano"
+  },
+  {
+    "nombre": "La Poesía",
+    "direccion": "Chile 502",
+    "barrio": "Monserrat"
+  },
+  {
+    "nombre": "Delicius Café",
+    "direccion": "Laprida 2015",
+    "barrio": "Recoleta"
+  },
+  {
+    "nombre": "London City",
+    "direccion": "Av, de Mayo 599",
+    "barrio": "San Nícolas"
+  }
+]
 var cont = document.getElementById("cont-res")
 
 for (let i = 0; i < cafeterias.length; i++) {
